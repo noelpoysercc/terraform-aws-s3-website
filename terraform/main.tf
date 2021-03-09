4c0bdacba8acb6929b6eaa115cc2a268d8f41dc5
noelpoysercc <69914943+noelpoysercc@users.noreply.github.com>
2021-03-09 17:38:50 -0500

    Initial commit of main.tf
    Initial commit of main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "s3Bucket" {
+  bucket = "vanguard-test-website"
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+  "Id": "MakePublic",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::vanguard-test-website/*",
+      "Principal": "*"
+    }
+  ]
+}
+EOF
+
+  website {
+    index_document = "Index.html"
+  }
+}
