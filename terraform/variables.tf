3a13869f03ea60cf3954597b2d2cd59b2fb7cf15
noelpoysercc <69914943+noelpoysercc@users.noreply.github.com>
2021-03-09 23:16:09 -0500

    Update variables.tf
    Another approach

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,33 +1,14 @@
+# Input variable definitions
+
 variable "bucket_name" {
-	description = "The name of the S3 bucket to create, e.g. your-bucket-name"
+  description = "Name of the s3 bucket. Must be unique."
+  type        = string
 }
 
-variable "deployer_arns" {
-  type        = "list"
-  default     = []
-  description = "(Optional) Array of deployer ARNs to grant `deployer` permissions"
+variable "tags" {
+  description = "Tags to set on the bucket."
+  type        = map(string)
+  default     = {}
 }
-
-variable duplicate_content_penalty_secret {
-  description = "Secret/password used to restrict access to S3 to CloudFront"
-}
-
-#variable environment {
-#  default = "default"
-#  description = "The label for the environment. Used for naming/tagging purposes"
-#}
-
-variable "error_document" {
-  default = "error.html"
-}
-
-variable "index_document" {
-  default = "index.html"
-}
-
-variable region {
-  description = "e.g. us-east-1"
-}
-
 # variable "aws_secret_key" {}
 # variable "aws_access_key" {}
