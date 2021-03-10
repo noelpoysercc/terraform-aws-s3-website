c6f120258ac1ad2876514c025946bfe851421c98
noelpoysercc <69914943+noelpoysercc@users.noreply.github.com>
2021-03-09 21:46:45 -0500

    Update output.tf
    Output bucket name and bucket domain name

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,4 +1,7 @@
-output "this_s3_bucket_bucket_domain_name" {
-  description = "The bucket domain name. Will be of format bucketname.s3.amazonaws.com."
-  value       = element(concat(aws_s3_bucket.this.*.bucket_domain_name, [""]), 0)
+output "s3_website_bucket_name" {
+  value = "${aws_s3_bucket.website_bucket.id}"
+}
+
+output "s3_website_bucket_domain_name" {
+  value = "${aws_s3_bucket.website_bucket.bucket_domain_name}"
 }
