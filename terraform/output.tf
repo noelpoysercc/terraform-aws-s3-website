53db28f8d2664bf8cf7b2c4e2e78b407972e0837
noelpoysercc <69914943+noelpoysercc@users.noreply.github.com>
2021-03-10 16:26:36 -0500

    Update output.tf
    added url

--- terraform/output.tf
+++ terraform/output.tf
@@ -14,3 +14,9 @@ output "domain" {
   description = "Domain name of the bucket"
   value       = aws_s3_bucket.s3_bucket.website_domain
 }
+
+output "url" {
+  description = "URL of the bucket"
+  value       = "${aws_s3_bucket.s3_bucket.bucket}.s3-website-${var.region}.amazonaws.com"
+}
+
