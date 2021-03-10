380b5069de9f8feec676fce304b174439d06d218
noelpoysercc <69914943+noelpoysercc@users.noreply.github.com>
2021-03-09 21:01:11 -0500

    Update variables.tf
    Add variable to determine if a new S3 bucket is to be created

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,2 +1,8 @@
+variable "create_bucket" {
+  description = "Controls if S3 bucket should be created"
+  type        = bool
+  default     = true
+}
+
 variable "aws_secret_key" {}
 variable "aws_access_key" {}
