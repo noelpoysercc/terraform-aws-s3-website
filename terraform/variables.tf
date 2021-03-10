af39861e373fc2ec57ca6ae519dce6b40450c211
noelpoysercc <69914943+noelpoysercc@users.noreply.github.com>
2021-03-09 21:52:44 -0500

    Update variables.tf
    Trying yet another approach

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,29 @@
-variable "create_bucket" {
-  description = "Conditionally create S3 bucket"
-  default     = true
+variable "bucket_name" {
+	description = "The name of the S3 bucket to create, e.g. your-bucket-name"
 }
 
-variable "upload_files" {
-  description = "Conditionally upload files"
-  default     = true
+variable "deployer_arns" {
+  type        = "list"
+  default     = []
+  description = "(Optional) Array of deployer ARNs to grant `deployer` permissions"
 }
 
-variable "allow_public" {
-  description = "Allow public read access to bucket"
-  default     = false
+#variable duplicate_content_penalty_secret {
+#  description = "Secret/password used to restrict access to S3 to CloudFront"
+#}
+
+#variable environment {
+#  default = "default"
+#  description = "The label for the environment. Used for naming/tagging purposes"
+#}
+
+variable "error_document" {
+  default = "error.html"
 }
 
-variable "aws_secret_key" {}
-variable "aws_access_key" {}
+variable "index_document" {
+  default = "index.html"
+}
+
+# variable "aws_secret_key" {}
+# variable "aws_access_key" {}
