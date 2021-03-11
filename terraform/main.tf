d6018f849b1175c19705b425b2febf83e4649053
noelpoysercc <69914943+noelpoysercc@users.noreply.github.com>
2021-03-10 23:54:20 -0500

    Update main.tf
    upload all files

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,13 +59,18 @@ resource "aws_s3_bucket_policy" "s3_bucket_policy" {
 EOF
 }
 
-resource "aws_s3_bucket_object" "index" {
+resource "aws_s3_bucket_object" "s3_upload" {
+  for_each = fileset("${path.root}/src", "**/*")
+  
   bucket = aws_s3_bucket.s3_bucket.id
-  key    = "index_object_key"
-  source = "index.html"
+  #key    = "index.html"
+  #source = "./src/index.html"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("index.html")
+  #etag = filemd5("index.html")
+  key    = each.value
+  source = "${path.root}/src/${each.value}"
+  etag   = filemd5("${path.root}/src/${each.value}")
 }
