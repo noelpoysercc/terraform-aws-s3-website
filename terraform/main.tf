6cf45a80c2026866711dd8c4017387c5a472cfda
noelpoysercc <69914943+noelpoysercc@users.noreply.github.com>
2021-03-10 23:08:13 -0500

    Update main.tf
    try to upload index.html

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,3 +58,14 @@ resource "aws_s3_bucket_policy" "s3_bucket_policy" {
 }
 EOF
 }
+
+resource "aws_s3_bucket_object" "index" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  key    = "index_object_key"
+  source = "index.html"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("../src")
+}
