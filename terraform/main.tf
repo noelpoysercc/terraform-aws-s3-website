dd3b651bc28654a5ef38051a0ae8c1a446b2bec7
noelpoysercc <69914943+noelpoysercc@users.noreply.github.com>
2021-03-10 22:59:03 -0500

    Update main.tf
    cleanup

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,17 +5,6 @@ terraform {
   required_version = ">= 0.12"
 }
 
-#module "website_s3_bucket" {
-#  source = "./vanguard-test-website"
-
-#  bucket_name = "<UNIQUE BUCKET NAME>"
-
-#  tags = {
-#    Terraform   = "true"
-#    Environment = "dev"
-#  }
-#}
-
 provider "random" {}
 
 # Create a random string
@@ -38,25 +27,6 @@ resource "aws_s3_bucket" "s3_bucket" {
   
   acl    = "public-read"
   
-#  policy = <<EOF
-#{
-#    "Version": "2012-10-17",
-#    "Statement": [
-#        {
-#            "Sid": "PublicReadGetObject",
-#            "Effect": "Allow",
-#            "Principal": "*",
-#            "Action": [
-#                "s3:GetObject"
-#            ],
-#            "Resource": [
-#                "arn:aws:s3:::${aws_s3_bucket.s3_bucket.id}/*"
-#            ]
-#        }
-#    ]
-#}
-#EOF
-
   website {
     index_document = "index.html"
     error_document = "error.html"
